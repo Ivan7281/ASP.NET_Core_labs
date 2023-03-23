8dececb185da737c49692f573e5fa614f041a2e2
Ivan7281 <demian.ivan@student.uzhnu.edu.ua>
2023-03-23 20:52:22 +0200

    lab1

---
+++ lab1/WebApplication1/WebApplication1/Models/EnterpriseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class EnterpriseContext : DbContext
+    {
+        public EnterpriseContext(DbContextOptions<EnterpriseContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Enterprise> Enterprises { get; set; } = null!;
+    }
+}
