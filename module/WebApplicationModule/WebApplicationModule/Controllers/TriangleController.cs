9c2192dbd6e237ddb860a676d8b6c82e9dd1e7ab
Ivan7281 <demian.ivan@student.uzhnu.edu.ua>
2023-03-30 17:18:12 +0300

    module

---
+++ module/WebApplicationModule/WebApplicationModule/Controllers/TriangleController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationModule.Controllers
+{
+    public class TriangleController : Controller
+    {
+        [HttpGet("task1")]
+        public async Task<ActionResult<double>> GetSin(double a, double b)
+        {
+            double c = Math.Sqrt(a * a + b * b);
+            double d = (a + b) / 2;
+            return Ok(new { c, d });
+        }
+        [HttpGet("task2")]
+        public async Task<ActionResult<double>> GetSin(int n)
+        {
+            bool f = true;
+            int num1;
+            int num2 = 0;
+            if (n < 10)
+            {
+                return BadRequest(400);
+            }
+            while (n > 0)
+            {
+                num1 = n % 10;
+                if (num1 < num2)
+                {
+                    f = false;
+                }
+                num2 = num1;
+                n /= 10;
+            }
+            return Ok(f);
+        }
+    }
+}
